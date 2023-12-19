bbf6e1073c7cc92a13dbc03bc324335b70bd702a
MGITSTEP <149448708+MGITSTEP@users.noreply.github.com>
2023-12-18 18:51:14 -0800

    Add files via upload

---
+++ Cocina.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+// Interfaz para los pasos del proceso
+interface Cocina {
+    void prepararIngredientes();
+    void cocinar();
+    void servir();
+}
+
+// Clase abstracta para los robots de cocina
+abstract class RobotCocina implements Cocina {
+    // Implementación común para todos los robots
+    public void prepararIngredientes() {
+        System.out.println("Robot preparando ingredientes...");
+    }
+
+    public void cocinar() {
+        System.out.println("Robot cocinando...");
+    }
+
+    public void servir() {
+        System.out.println("Robot sirviendo el plato.");
+    }
+
+    // Método abstracto para la acción específica de cada tipo de robot
+    public abstract void realizarAccion();
+}
+
+// Clases concretas para los diferentes tipos de robots
+class RobotDesayuno extends RobotCocina {
+    public void realizarAccion() {
+        System.out.println("Robot de desayuno preparando café.");
+    }
+}
+
+class RobotComida extends RobotCocina {
+    public void realizarAccion() {
+        System.out.println("Robot de comida preparando almuerzo.");
+    }
+}
+
+class RobotCena extends RobotCocina {
+    public void realizarAccion() {
+        System.out.println("Robot de cena preparando cena.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selecciona una opción:");
+        System.out.println("1. Desayuno");
+        System.out.println("2. Comida");
+        System.out.println("3. Cena");
+
+        int opcionMenu = scanner.nextInt();
+
+        RobotCocina robot = null;
+
+        switch (opcionMenu) {
+            case 1:
+                robot = new RobotDesayuno();
+                break;
+            case 2:
+                robot = new RobotComida();
+                break;
+            case 3:
+                robot = new RobotCena();
+                break;
+            default:
+                System.out.println("Opción no válida");
+                System.exit(1);
+        }
+
+        robot.realizarAccion();
+
+        System.out.println("Selecciona el siguiente paso:");
+        System.out.println("1. Preparar ingredientes");
+        System.out.println("2. Cocinar");
+        System.out.println("3. Servir");
+
+        int Paso = scanner.nextInt();
+
+        switch (Paso) {
+            case 1:
+                robot.prepararIngredientes();
+                break;
+            case 2:
+                robot.cocinar();
+                break;
+            case 3:
+                robot.servir();
+                System.out.println("¡Hurra! El plato estaba delicioso.");
+                break;
+            default:
+                System.out.println("Opción no válida");
+                System.exit(1);
+        }
+
+        scanner.close();
+    }
+}
